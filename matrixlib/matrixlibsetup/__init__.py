5bd32b0f071e29a81fe733d4f78bad145b5d7d0f
SahasT23 <sahas.st01@gmail.com>
2025-07-07 08:52:57 +0100

    Have to restart

--- matrixlib/matrixlibsetup/__init__.py
+++
@@ -1,8 +0,0 @@
-from .matrix import matrix_multiply, inverse, determinant, dot_product, cross_product
-from .matrix import add, subtract, solve, scalar_multiply, transpose
-from .utils import is_square, is_vector
-
-__all__ = [
-    'matrix_multiply', 'inverse', 'determinant', 'dot_product', 'cross_product',
-    'add', 'subtract', 'solve', 'scalar_multiply', 'transpose', 'is_square', 'is_vector'
-]
--- matrixlib/matrixlibsetup/utils.py
+++
@@ -1,7 +0,0 @@
-def is_square(matrix):
-    """Check if a matrix is square."""
-    return len(matrix) > 0 and len(matrix) == len(matrix[0])
-
-def is_vector(matrix):
-    """Check if input is a vector (1D matrix)."""
-    return len(matrix) > 0 and isinstance(matrix[0], (int, float))
